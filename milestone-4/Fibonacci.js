ea81277b505d8abbf614643e8c093ea365282dc7
Asaf Gilboa <asafg86@gmail.com>
2022-06-22 11:40:12 +0300

    milestone4 first commit

---
+++ milestone-4/Fibonacci.js
@@ -0,0 +1,31 @@
+/* 
+ * ITC Full-Stack Bootcamp
+ * Fibonacci assignment
+ * 19/06/2022
+ * Asaf Gilboa
+ */
+
+
+async function fiboFetch(fiboServer) {
+  const response = await fetch(fiboServer);
+  const fibo = await response.json();
+  return fibo.result;
+}
+
+async function fiboServerCall(num) {
+  let fiboServer = "http://localhost:5050/fibonacci/";
+  fiboServer += num;
+  fiboFetch(fiboServer).then(data => {
+    document.getElementById("result").innerHTML = (`<u><b>${data}</b></u>`);
+  });
+}
+
+
+document.getElementById("getY").addEventListener("click", function (e) {
+
+  document.getElementById("result").innerHTML = '';
+  const x = document.getElementById("inputX").value;
+
+  fiboServerCall(x);
+
+});
