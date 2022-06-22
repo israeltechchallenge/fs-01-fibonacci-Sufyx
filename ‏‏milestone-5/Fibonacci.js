a3f746bb4a2d817c6ecde2e715a600c4f8685e46
Asaf Gilboa <asafg86@gmail.com>
2022-06-22 22:32:55 +0300

    milestone5 pre staging

--- ‏‏milestone-5/Fibonacci.js
+++ ‏‏milestone-5/Fibonacci.js
@@ -11,7 +11,8 @@ async function fiboFetch(fiboServer) {
   const response = await fetch(fiboServer);
   if (!response.ok) {
     const err = await response.text();
-    return err;
+    throw Error(err);
+    // return err;
   }
   const fibo = await response.json();
   return fibo.result;
@@ -20,21 +21,23 @@ async function fiboFetch(fiboServer) {
 async function fiboServerCall(num) {
   let fiboServer = "http://localhost:5050/fibonacci/";
   fiboServer += num;
+  let res = document.getElementById("result");
 
   fiboFetch(fiboServer).then(data => {
-    let res = document.getElementById("result");
     res.style.display = "initial";
-    document.getElementById("loader").style.display = "none";
+    document.getElementById("loader").classList.add("d-none");
 
-    if (isNaN(data)) {
-      res.classList.remove("serverResult");
-      res.classList.add("text-danger");
-      res.innerHTML = (`Server Error: ${data}`);
-    } else {
-      res.classList.add("serverResult");
-      res.classList.remove("text-danger");
-      res.innerHTML = (`<u><b>${data}</b></u>`);
-    }
+    res.classList.add("fs-4");
+    res.classList.remove("text-danger");
+    res.innerHTML = (`<u><b>${data}</b></u>`);
+    
+  }).catch(error => {
+    res.style.display = "initial";
+    document.getElementById("loader").classList.add("d-none");
+    console.log(error);
+    res.classList.remove("fs-4");
+    res.classList.add("text-danger");
+    res.innerHTML = (`Server Error: ${error}`);
   });
 }
 
@@ -52,7 +55,8 @@ document.getElementById("getY").addEventListener("click", function (e) {
     let message = ( (x > 50) ? "Can't be larger than 50" : "Can't be less than 1");
     document.getElementById("message").innerHTML = message;
   } else {
-    document.getElementById("loader").style.display = "initial";
+    // document.getElementById("loader").style.display = "initial";
+    document.getElementById("loader").classList.remove("d-none");
     fiboServerCall(x);
   }
 
