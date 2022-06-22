75658ad1189735436a4f615ae19685a62e7c3bfd
Asaf Gilboa <54150314+Sufyx@users.noreply.github.com>
2022-06-22 20:44:52 +0300

    Update Fibonacci.js
    switched to fuul bootstrap styling

--- milestone-3/Fibonacci.js
+++ milestone-3/Fibonacci.js
@@ -7,25 +7,26 @@
 
 
 document.getElementById("getY").addEventListener("click", function(e) {
-    document.getElementById("message").innerHTML = "";
     const x = document.getElementById("inputX").value;
 
-    if ((isNaN(x)) || (x < 0) || (x != parseInt(x)) || (x > 30)) {
-        document.getElementById("message").innerHTML = "Please enter a whole number between 0 and 30"; 
+    if ((isNaN(x)) || (x < 0) || (x != parseInt(x)) || (x > 100)) {
+        document.getElementById("result").innerHTML = "Illegal input"
         return;
     }
-
     const y = fibonacciRecursion(x);
-
     document.getElementById("result").innerHTML = y;
 });
 
 
-function fibonacciRecursion(x) { 
-    if (x <= 1) {
-        return x;
+function fibonacciRecursion(x, a = 0, b = 1)
+{
+    if (x === 0){
+        return a;
     }
-    return (fibonacciRecursion(x-1) + fibonacciRecursion(x-2));
+    if (x === 1){
+        return b;
+    }
+    return fibonacciRecursion(x - 1, b, a + b);
 }
 
 
@@ -43,13 +44,4 @@ function fibonacciOfX(x) {
         n2 = n3;
     }
     return n3;
-}
-
-//   x=?
-let num1 = 0;
-let num2 = 1;
-let num3;
-for (let i = 2; i <= x; i++) {
-    num3 = num1 + num2;
-    
 }
